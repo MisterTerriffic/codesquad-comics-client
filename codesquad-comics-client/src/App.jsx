5b313f06c208e6017a3b3f410e5a7b78f100db81
Walter Fisher <o.b.i.e@Mac.lan>
2025-04-06 17:34:07 -0500

    import the useState create a new state user, pass down the user and seter function to header, login, and signup.

--- codesquad-comics-client/src/App.jsx
+++ codesquad-comics-client/src/App.jsx
@@ -13,17 +13,20 @@ import Header from  './shared/header'
 import Footer from  './shared/footer'
 
 function App() {
+  const [user, setUser] = useState(1);
+  localStorage.setItem("user", user);
 
   return (
     <>
       <div>
-      <Header/>
+        
+      <Header user={user} setUser={setUser}/>
       <Home/>
       <About/>
       <Admin/>
       <Create/>
-      <Login/>
-      <SingUp/>
+      <Login user={user} setUser={setUser}/>
+      <SingUp user={user} setUser={setUser}/>
       <Update/>
       <Footer/>
       </div>
